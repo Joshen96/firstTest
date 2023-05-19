313d1a145c180a310a226b0da3a08da450aa0b74
WinterDotori <sying14@naver.com>
2023-05-19 10:42:07 +0900

    BasketballShoot

--- Assets/Seyoung/Script/Ball_sy.cs
+++ Assets/Seyoung/Script/Ball_sy.cs
@@ -4,11 +4,28 @@ using UnityEngine;
 
 public class Ball_sy : MonoBehaviour
 {
-    // 어느 물건에 맞았는지 확인
-    // 골대 트리거가 아닌 부분에 맞았다면 알림뜨기 "안들어갔지롱~~~"
-    // 골대 트리거에 맞았다면 회전하다가 들어가기
-    // 골대 천부분까지 공이 들어갔다면 성공이니까 Manager에게 성공했다고 알려주거
-    // 성공 소식을 들은 Manager은 성공코드 시행 
-    // 성공코드에는 전광판 점수 올리기 or 성공 파티클 터뜨리기ㅐ or 성공 소리 on 등등을 넣을 수 있음
+    private BallManager_sy ballManager = null;
 
+    private void Start()
+    {
+        ballManager = transform.parent.GetComponent<BallManager_sy>();
+    }
+
+    public void OnTriggerEnter(Collider other) 
+    {
+        switch (other.gameObject.name)
+        {
+            case "GoalLineTrigger":
+                {
+                    // 공이 회전하다가 들어가기 (-)
+                }
+                break;
+
+            case "GoalInTrigger":
+                {
+                    ballManager.GoalIn();               // 골인했다고 알려주기
+                }
+                break;
+        }
+    }
 }
