4a50c1d691b8dd482f02dc8e48c0fb59b429b75d
Joshen96 <asdzxc250@naver.com>
2023-05-24 14:26:13 +0900

    칠판

---
+++ Assets/Joshen/Joshentest/Whiteboard/JS_WhiteBoard.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JS_WhiteBoard : MonoBehaviour
+{
+    public Texture2D texture;
+
+    public Vector2 textureSize = new Vector2(2048, 2048);
+
+    void Start()
+    {
+        var r = GetComponent<Renderer>();
+        texture = new Texture2D((int)textureSize.x, (int)textureSize.y);
+        r.material.mainTexture = texture;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
