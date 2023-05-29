423483972056fba4f5a5422d800abd3c01d9afd3
Joshen96 <asdzxc250@naver.com>
2023-05-29 18:06:48 +0900

    문 열쇠구현

---
+++ Assets/Joshen/Joshentest/door/JS_Keysoket.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JS_Keysoket : MonoBehaviour
+{
+    [SerializeField]
+    GameObject openUI;
+    [SerializeField]
+    GameObject lockUI;
+    [SerializeField]
+    GameObject key;
+    [SerializeField]
+    Transform keysoket;
+
+    private void Start()
+    {
+        openUI.SetActive(false);
+        lockUI.SetActive(true);
+    }
+
+   
+    public void onon()
+    {
+        openUI.SetActive(true);
+        lockUI.SetActive(false);
+    }
+    public void lockdoor()
+    {
+        openUI.SetActive(false);
+        lockUI.SetActive(true);
+    }
+
+}
