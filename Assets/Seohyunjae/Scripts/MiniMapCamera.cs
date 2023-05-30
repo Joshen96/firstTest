673053f1b7fff9b91ed580d8342363ff456a1f74
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-05-30 15:31:08 +0900

    미니맵구현하는중

--- Assets/Seohyunjae/Scripts/MiniMapCamera.cs
+++ Assets/Seohyunjae/Scripts/MiniMapCamera.cs
@@ -5,27 +5,26 @@ using UnityEngine;
 
 public class MiniMapCamera : MonoBehaviour
 {
-    public Transform target; // 카메라가 따라다닐 대상의 Transform
+    public Transform target; // 캐릭터의 Transform 참조
+    public float distance = 10f; // 카메라와 캐릭터 사이의 거리
 
-    public float smoothSpeed = 0.125f; // 카메라 이동 속도
-    public Vector3 offset; // 카메라와 대상 간의 오프셋
 
-    private Vector3 originalPosition; // 카메라의 초기 위치
+    private GameObject characterDot; // 캐릭터 점 객체
 
-    private void Start()
-    {
-        originalPosition = transform.position;
-    }
+
 
     private void LateUpdate()
     {
-        Vector3 desiredPosition = target.position + offset;
-        desiredPosition.y = originalPosition.y; // 카메라의 y 위치를 고정
-        desiredPosition.z = originalPosition.z; // 카메라의 z 위치를 고정
-        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
-        transform.position = smoothedPosition;
+        // 캐릭터의 위치에 따라 카메라 위치 및 캐릭터 점 위치 업데이트
+        Vector3 targetPosition = target.position;
+        Vector3 cameraPosition = targetPosition - distance * Vector3.forward;
+        cameraPosition.y = 40f; // 카메라의 높이 고정
+        transform.position = cameraPosition;
 
-        transform.rotation = Quaternion.Euler(90f, target.eulerAngles.y, 0f); // 카메라를 위쪽으로 회전
+        // 카메라의 회전을 고정
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f);
+
+
     }
 }
 
