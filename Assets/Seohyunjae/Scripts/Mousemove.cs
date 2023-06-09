cd53a36a56e17f1a7edff6ec93c5f2a6aeeb6637
seohyunjae <97527644+seohyunjae@users.noreply.github.com>
2023-06-09 16:53:07 +0900

    6/9 동물구현완성

--- Assets/Seohyunjae/Scripts/Mousemove.cs
+++ Assets/Seohyunjae/Scripts/Mousemove.cs
@@ -1,35 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
-public class Catmove : MonoBehaviour
+public class Mousemove : MonoBehaviour
 {
 
-    public Transform cat;
+    public Transform mouse;
     public Transform[] paths;
     public Transform Player;
     public Animator animator;
-    public float time = 0f;
 
 
     public string[] animationStates;
-    public float speed = 1.5f;
-    public float scrPlayDist = 8.0f;
+    public float speed = 2.5f;
+    public float scrPlayDist = 3f;
     public int i = 0;
     private bool isclicked = false;
     private bool aniselect = false;
 
-    
+
     // Start is called before the first frame update
 
     private void Start()
     {
-
-        cat.transform.position = paths[0].transform.position;
+        mouse.transform.position = paths[0].transform.position;
         animator = GetComponent<Animator>();
-        
 
         if (animator == null || animationStates.Length == 0)
             return;
@@ -38,30 +33,30 @@ public class Catmove : MonoBehaviour
     }
     public void Update()
     {
-        
+
         float dist = CalcDistanceWithTarget();
-        // 거리가 가까우면
         if (dist < scrPlayDist)
         {
+            //animator.SetBool("Spin", !isclicked);
             transform.LookAt(Player.position);
-
+            //mouseangleturn.transform.rotation = Quaternion.Euler(Vector3.right * 180f);
+            //Debug.Log(Quaternion.Euler(Vector3.right * dist));
             if (!aniselect)
             {
                 PlayRandomAnimation();
             }
-          
-            
-            
-            //catangleturn.transform.rotation = Quaternion.Euler(Vector3.right * 180f);
-            //Debug.Log(Quaternion.Euler(Vector3.right * dist));
+            animator.Play("Entry");
+
         }
         else
         {
             aniselect = false;
             animator.Play("Walk");
-            cat.transform.position = Vector3.MoveTowards(cat.transform.position, paths[i].transform.position, speed * Time.deltaTime);
+
+
+            mouse.transform.position = Vector3.MoveTowards(mouse.transform.position, paths[i].transform.position, speed * Time.deltaTime);
             lookingPlayer(paths[i]);
-            if (cat.transform.position == paths[i].transform.position)
+            if (mouse.transform.position == paths[i].transform.position)
             {
                 i++;
             }
@@ -74,34 +69,33 @@ public class Catmove : MonoBehaviour
     }
     public void lookingPlayer(Transform _nextpath)
     {
-        Vector3 dir = _nextpath.transform.position - cat.transform.position;
-        cat.gameObject.transform.rotation = Quaternion.Lerp(cat.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 10f);
+        Vector3 dir = _nextpath.transform.position - mouse.transform.position;
+        mouse.gameObject.transform.rotation = Quaternion.Lerp(mouse.transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * 10f);
 
     }
-  
+
     private float CalcDistanceWithTarget()
     {
         Vector3 dirToTarget =
-            Player.position - cat.transform.position;
+            Player.position - mouse.transform.position;
         float dist = dirToTarget.magnitude;
 
         dist = Vector3.Distance(
-            Player.position, cat.transform.position);
+            Player.position, mouse.transform.position);
 
         return dist;
     }
 
     private void PlayRandomAnimation()
     {
-       
+
         if (animator != null && animationStates.Length > 0)
         {
             int randomIndex = Random.Range(0, animationStates.Length);
             string randomState = animationStates[randomIndex];
-            animator.Play(randomState,-1,1f);
+            animator.Play(randomState, -1, 1f);
             aniselect = true;
         }
     }
-   
 
 }
