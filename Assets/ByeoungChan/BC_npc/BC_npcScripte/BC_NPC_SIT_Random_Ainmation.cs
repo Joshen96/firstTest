b88faea2e54ecfd1437fa91f52631660d85a38ff
byeoungchankim <kbc625@naver.com>
2023-06-13 18:06:20 +0900

    Revert "Merge pull request #198 from Joshen96/byeoungchan"
    This reverts commit fb5ddd052b627ade01be732bd1d1dfadc23c1b1e, reversing
    changes made to 1240977987e70ee49a6016a22ba01ddb434e46d6.

--- Assets/ByeoungChan/BC_npc/BC_npcScripte/BC_NPC_SIT_Random_Ainmation.cs
+++ Assets/ByeoungChan/BC_npc/BC_npcScripte/BC_NPC_SIT_Random_Ainmation.cs
@@ -8,13 +8,10 @@ public class BC_NPC_SIT_Random_Ainmation : MonoBehaviour
     public string SitOneAnimationName = "Sitting";
     public string SitTwoAnimationName = "SittingTwo";
     public string SitThreeAnimationName = "SittingThree";
-    public string SitTypingAnimationName = "Typing";
-    public string SitBoringAnimationName = "Boring Sitting Pose";
-    
     public float SitAnimationProbability = 0.5f; // Phone 애니메이션의 확률 (0부터 1 사이)
-    /*public float SitAnimationSpeed = 1.5f;*/ // Phone 애니메이션의 재생 속도
-    public float minAnimationTime = 5f; // 최소 애니메이션 재생 시간
-    public float maxAnimationTime = 10f; // 최대 애니메이션 재생 시간
+    public float SitAnimationSpeed = 1.5f; // Phone 애니메이션의 재생 속도
+    public float minAnimationTime = 2f; // 최소 애니메이션 재생 시간
+    public float maxAnimationTime = 5f; // 최대 애니메이션 재생 시간
 
     private float nextAnimationTime;
 
@@ -46,8 +43,6 @@ public class BC_NPC_SIT_Random_Ainmation : MonoBehaviour
         {
             SitPhoneAnimation();
             PlayWalkAnimation();
-            BoringAnimation();
-            TypingAnimation();
         }
         
 
@@ -68,40 +63,13 @@ public class BC_NPC_SIT_Random_Ainmation : MonoBehaviour
         // "phone" 애니메이션 재생
         animator.SetBool("isSitTwoAnimation", true);
         animator.Play(SitTwoAnimationName); 
-        
+        animator.speed = SitAnimationSpeed;
     }
     private void SitPhoneAnimation()
     {
         // "phone" 애니메이션 재생
         animator.SetBool("isSitThreeAnimation", true);
         animator.Play(SitTwoAnimationName);
-        
-    }
-    //private void SleepAnimation()
-    //{
-    //    // "phone" 애니메이션 재생
-    //    animator.SetBool("isSleepAnimation", true);
-    //    animator.Play(SleepAnimationAnimationName);
-
-    //}
-    private void BoringAnimation()
-    {
-        // "phone" 애니메이션 재생
-        animator.SetBool("isBoringAnimation", true);
-        animator.Play(SitBoringAnimationName);
-
-    }
-    private void TypingAnimation()
-    {
-        // "phone" 애니메이션 재생
-        animator.SetBool("isSitTypingAnimation", true);
-        animator.Play(SitTypingAnimationName);
-        Invoke(nameof(StopTypingAnimation), Random.Range(minAnimationTime, maxAnimationTime));
-
-    }
-
-    private void StopTypingAnimation()
-    {
-        animator.SetBool("isSitTypingAnimation", false);
+        animator.speed = SitAnimationSpeed;
     }
 }
